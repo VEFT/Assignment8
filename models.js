516c490047dcdf3034291c484c76ab5dee716d01
danben4 <danielbenediktsson4@gmail.com>
2015-10-17 16:01:09 +0000

    flot

--- models.js
+++ models.js
@@ -3,12 +3,58 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    token: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        maxlength: 1,
+        minlength: 1
+    },
 });
 
 const CompanySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlenght: 1
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    punchcard_lifetime {
+        type: Number,
+        required: true
+    }
 });
 
 const PunchcardSchema = mongoose.Schema({
+    company_id: {
+        type: Number,
+        required: true
+    },
+    user_id: {
+        type: Number,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 module.exports = {
@@ -16,3 +62,4 @@ module.exports = {
     Company: mongoose.model('Company', CompanySchema),
     Punchcard: mongoose.model('Punchcard', PunchcardSchema)
 };
+
